6d0241a63655e68267a005d6bef9c4a458ea2a7a
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-06-21 17:43:38 +0530

    improved formatting

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,118 +1,117 @@
-class calculator{
-    constructor (previouselement,currentelement){
-        this.previouselement = previouselement
-        this.currentelement = currentelement
-        this.clear()
-    }
+class calculator {
+  constructor(previouselement, currentelement) {
+    this.previouselement = previouselement;
+    this.currentelement = currentelement;
+    this.clear();
+  }
 
-    clear(){
-        this.current = ''
-        this.previous = ''
-        this.operation = undefined
+  clear() {
+    this.current = "";
+    this.previous = "";
+    this.operation = undefined;
+  }
+  delete() {
+    this.current = this.current.toString().slice(0, -1);
+  }
+  append(number) {
+    if (number === "." && this.current.includes(".")) return;
+    this.current = this.current.toString() + number.toString();
+  }
+  chooseopr(operation) {
+    if (this.current === "") return;
+    if (this.previous != "") {
+      this.compute();
     }
-    delete(){
-        this.current = this.current.toString().slice(0,-1)
+    this.operation = operation;
+    this.previous = this.current;
+    this.current = "";
+  }
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previous);
+    const cur = parseFloat(this.current);
+    if (isNaN(prev) || isNaN(cur)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + cur;
+        break;
+      case "-":
+        computation = prev - cur;
+        break;
+      case "÷":
+        computation = prev / cur;
+        break;
+      case "*":
+        computation = prev * cur;
+        break;
+      default:
+        return;
     }
-    append(number){
-        if (number === "." && this.current.includes(".")) return 
-        this.current = this.current.toString() + number.toString()
+    this.current = computation;
+    this.operation = undefined;
+    this.previous = "";
+  }
+  getdisplay(number) {
+    const stringnum = number.toString();
+    const integerdigit = parseFloat(stringnum.split(".")[0]);
+    const decimaldigit = stringnum.split(".")[1];
+    let integerdisplay;
+    if (isNaN(integerdigit)) {
+      integerdisplay = "";
+    } else {
+      integerdisplay = integerdigit.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
     }
-    chooseopr(operation){
-        if(this.current === "")return
-        if(this.previous != ""){
-            this.compute()
-        }
-        this.operation = operation
-        this.previous = this.current
-        this.current = ""
+    if (decimaldigit != null) {
+      return `${integerdisplay}.${decimaldigit}`;
+    } else {
+      return integerdisplay;
     }
-    compute(){
-        let computation
-        const prev  = parseFloat(this.previous)
-        const cur = parseFloat(this.current)
-        if (isNaN(prev) || isNaN(cur))return
-        switch (this.operation){
-            case "+":
-                computation = prev + cur
-                break
-            case "-":
-                computation = prev - cur
-                break
-            case "÷":
-                computation = prev / cur
-                break
-            case "*":
-                computation = prev * cur
-                break
-            default:
-                return
-        }
-        this.current = computation
-        this.operation = undefined
-        this.previous = ""
+  }
+  updatedisplay() {
+    this.currentelement.innerText = this.getdisplay(this.current);
+    if (this.operation != null) {
+      this.previouselement.innerText = `${this.getdisplay(this.previous)} ${
+        this.operation
+      }`;
+    } else {
+      this.previouselement.innerText = "";
     }
-    getdisplay(number){
-        const stringnum = number.toString()
-        const integerdigit = parseFloat(stringnum.split('.')[0])
-        const decimaldigit = stringnum.split('.')[1]
-        let integerdisplay 
-        if(isNaN(integerdigit)){
-            integerdisplay = ''
-        } else{
-            integerdisplay = integerdigit.toLocaleString('en',{
-                maximumFractionDigits:0
-            })
-        }
-        if(decimaldigit!=null){
-            return `${integerdisplay}.${decimaldigit}`
-        }else{
-            return integerdisplay
-        }
-    }
-    updatedisplay(){
-        this.currentelement.innerText = this.getdisplay(this.current)
-        if(this.operation!= null){
-            this.previouselement.innerText = `${this.getdisplay(this.previous)} ${this.operation}`
-        }
-        else{
-            this.previouselement.innerText = ''
-        }
-    }
+  }
 }
 
+const numberbuttons = document.querySelectorAll("[data-number]");
+const operationbuttons = document.querySelectorAll("[data-operation]");
+const allclearbutton = document.querySelector("[data-all-clear]");
+const deletebutton = document.querySelector("[data-delete]");
+const equalsbutton = document.querySelector("[data-equals]");
+const previouselement = document.querySelector("[data-previous]");
+const currentelement = document.querySelector("[data-current]");
 
-const numberbuttons = document.querySelectorAll('[data-number]')
-const operationbuttons = document.querySelectorAll('[data-operation]')
-const allclearbutton = document.querySelector('[data-all-clear]')
-const deletebutton = document.querySelector('[data-delete]')
-const equalsbutton = document.querySelector('[data-equals]')
-const previouselement = document.querySelector('[data-previous]')
-const currentelement = document.querySelector('[data-current]')
+const Calc = new calculator(previouselement, currentelement);
 
-
-const Calc = new calculator(previouselement,currentelement)
-
-numberbuttons.forEach(button => {
-    button.addEventListener('click',() => {
-        Calc.append(button.innerText)
-        Calc.updatedisplay()
-    })
-})
-operationbuttons.forEach(button => {
-    button.addEventListener('click',() => {
-        Calc.chooseopr(button.innerText)
-        Calc.updatedisplay()
-    })
-})
-equalsbutton.addEventListener('click', button => {
-    Calc.compute()
-    Calc.updatedisplay()
-})
-allclearbutton.addEventListener('click', button => {
-    Calc.clear()
-    Calc.updatedisplay()
-})
-deletebutton.addEventListener('click', button => {
-    Calc.delete()
-    Calc.updatedisplay()
-})
+numberbuttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    Calc.append(button.innerText);
+    Calc.updatedisplay();
+  });
+});
+operationbuttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    Calc.chooseopr(button.innerText);
+    Calc.updatedisplay();
+  });
+});
+equalsbutton.addEventListener("click", (button) => {
+  Calc.compute();
+  Calc.updatedisplay();
+});
+allclearbutton.addEventListener("click", (button) => {
+  Calc.clear();
+  Calc.updatedisplay();
+});
+deletebutton.addEventListener("click", (button) => {
+  Calc.delete();
+  Calc.updatedisplay();
+});
--- day 6/tha.js
+++ day 6/tha.js
@@ -1,61 +1,58 @@
 // Q1
-function is_array(gg){
-    return gg.constructor === Array
+function is_array(gg) {
+  return gg.constructor === Array;
 }
-console.log(is_array('w3resource'));
+console.log(is_array("w3resource"));
 console.log(is_array([1, 2, 4, 0]));
-console.log("--------------------------------------------")
+console.log("--------------------------------------------");
 
 // Q2
-array_Clone = (gg)=>{
-    return JSON.parse(JSON.stringify(gg));
-}
+array_Clone = (gg) => {
+  return JSON.parse(JSON.stringify(gg));
+};
 console.log(array_Clone([1, 2, 4, 0]));
 console.log(array_Clone([1, 2, [4, 0]]));
-console.log("--------------------------------------------")
+console.log("--------------------------------------------");
 
 // Q3
-first = (gg,n = 0)=>{
-    if (n == 0){
-        return gg[0]
-    }
-    if (n<0){
-        return[]
-    }
-    return gg.slice(0,n)
-}
+first = (gg, n = 0) => {
+  if (n == 0) {
+    return gg[0];
+  }
+  if (n < 0) {
+    return [];
+  }
+  return gg.slice(0, n);
+};
 console.log(first([7, 9, 0, -2]));
-console.log(first([],3));
-console.log(first([7, 9, 0, -2],3));
-console.log(first([7, 9, 0, -2],6));
-console.log(first([7, 9, 0, -2],-3));
-console.log("--------------------------------------------")
+console.log(first([], 3));
+console.log(first([7, 9, 0, -2], 3));
+console.log(first([7, 9, 0, -2], 6));
+console.log(first([7, 9, 0, -2], -3));
+console.log("--------------------------------------------");
 
 // Q4
 myColor = ["Red", "Green", "White", "Black"];
-console.log(myColor.join(','))
-console.log(myColor.join('+'))
-console.log("--------------------------------------------")
+console.log(myColor.join(","));
+console.log(myColor.join("+"));
+console.log("--------------------------------------------");
 
 // Q5
 let gg = {};
-var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
-for(let i = 0;i<arr1.length;i++){
-    if(!(arr1[i] in gg)){
-        gg[arr1[i]] = 1
-    }
-    else{
-        gg[arr1[i]] += 1
-    }
+var arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+for (let i = 0; i < arr1.length; i++) {
+  if (!(arr1[i] in gg)) {
+    gg[arr1[i]] = 1;
+  } else {
+    gg[arr1[i]] += 1;
+  }
 }
-let key = 0
-let max = 1
-for(keyy in gg){
-    if(gg[keyy]>max){
-        max = gg[keyy]
-        key = keyy
-    }
+let key = 0;
+let max = 1;
+for (keyy in gg) {
+  if (gg[keyy] > max) {
+    max = gg[keyy];
+    key = keyy;
+  }
 }
-console.log({[key] : max})
-
-
+console.log({ [key]: max });
