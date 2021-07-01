5e9865673e76cf5d58dba7478b32a46a1af159ab
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-07-02 00:26:40 +0530

    completed day 10 Tha

--- day 10/memory.js
+++ day 10/memory.js
@@ -1,7 +1,39 @@
-const grid = document.getElementById("grid");
+const cards = document.querySelectorAll(".memory-card");
 
-for (let i = 0; i < 36; i++) {
-  card = document.createElement("div");
-  card.className = "demo";
-  grid.appendChild(card)
+let locked = false;
+let hasflippedcard = false;
+let firstCard, secondCard;
+
+function flipcard() {
+  if (locked) return;
+  if (this === firstCard) return;
+  this.classList.toggle("flip");
+
+  if (!hasflippedcard) {
+    hasflippedcard = true;
+    firstCard = this;
+    // console.log({firstCard})
+  } else {
+    hasflippedcard = false;
+    secondCard = this;
+    // console.log(secondCard)
+    checkforeach();
+  }
 }
+
+function checkforeach() {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    firstCard.removeEventListener("click", flipcard);
+    secondCard.removeEventListener("click", flipcard);
+  } else {
+    locked = true;
+    setTimeout(() => {
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
+
+      locked = false;
+    }, 1500);
+  }
+}
+
+cards.forEach((card) => card.addEventListener("click", flipcard));
