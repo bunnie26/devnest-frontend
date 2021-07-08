6e26abbfc6e0293911804d755122241523653ab5
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-07-08 12:07:39 +0530

    completed day 16 card

---
+++ day 16/card/src/components/card.js
@@ -0,0 +1,30 @@
+const card = () => {
+  return (
+    <div className="container">
+  <div className="card">
+    <div className="imgBx">
+      <img src="https://assets.codepen.io/4164355/shoes.png" />
+    </div>
+    <div className="contentBx">
+      <h2>Nike Shoes</h2>
+      <div class="size">
+        <h3>Size :</h3>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span>10</span>
+      </div>
+      <div className="color">
+        <h3>Color :</h3>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <a href="#">Buy Now</a>
+    </div>
+  </div>
+</div>
+  );
+};
+
+export default card
--- day 16/chess/src/components/card.js
+++
@@ -1,9 +0,0 @@
-const card = () => {
-  return (
-    <div>
-      <h2>React card</h2>
-      <p>this is a react card</p>
-    </div>
-  );
-};
-export default card;
