52df2d59da34863f2d512ed7048b30d38e661716
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-07-16 00:21:13 +0530

    day 20 completed

--- day_20/src/App.js
+++ day_20/src/App.js
@@ -1,44 +1,58 @@
 import "./App.css";
 import Card from "./components/card";
+import { useState } from "react";
 
 function App() {
-  const stored = [
+  const [value, setValue] = useState([
     {
+      id: 1,
       title: "Pizza",
       description: "No. of Cals consumed today :",
       val: 56,
       img: "https://imgur.com/PPF5hRK.png",
     },
     {
+      id: 2,
       title: "Burger",
       description: "No. of Cals consumed today :",
       val: 100,
       img: "https://imgur.com/Ce2Wl9b.png",
     },
     {
+      id: 3,
       title: "Coke",
       description: "No. of Cals consumed today :",
       val: 206,
       img: "https://imgur.com/fNLZb6M.png",
     },
     {
+      id: 4,
       title: "Fried Rice",
       description: "No. of Cals consumed today :",
       val: 178,
       img: "https://imgur.com/8z5hDHt.png",
     },
-  ];
-  
+  ]);
+
+  const deleteFun = (id) => {
+    const newArray = value.filter((omk) => omk.id !== id);
+    setValue(newArray);
+  };
+
   return (
-    <div >
-      <h1 className = "gg">Calory Tracker</h1>
+    <div>
+      <h1 className="gg">Calory Tracker</h1>
       <div className="App">
-        {stored.map((i) => (
+        {value.map((i) => (
           <Card
             title={i.title}
             description={i.description}
             val={i.val}
             img={i.img}
+            id={i.id}
+            deleteFun={() => {
+              deleteFun(i.id);
+            }}
           />
         ))}
       </div>
--- day_20/src/components/card.js
+++ day_20/src/components/card.js
@@ -1,12 +1,14 @@
-const Card = ({ title, img, val, description }) => {
+const Card = ({ title, img, val, description, id, deleteFun }) => {
   return (
     <div className="Card">
-      <div className = "demo">
+      <div className="demo">
         <div className="image-container">
           <h1>{title}</h1>
           <img src={img}></img>
         </div>
-        <button className = "btn" ><i class = "fas fa-trash"></i></button>
+        <button className="btn" onClick={() => deleteFun(id)}>
+          <i class="fas fa-trash"></i>
+        </button>
       </div>
       <p className={val > 100 ? "red" : "green"}>
         {description} {val}
