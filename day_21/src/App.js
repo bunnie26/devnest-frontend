7b5d6c580b47c7d94687ce026637e04b8fd3a16c
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-07-17 00:45:14 +0530

    completed basic sctricture

--- day_21/src/App.js
+++ day_21/src/App.js
@@ -3,26 +3,34 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState(["how you doin"]);
+  const [todos, setTodos] = useState([]);
   const addTodo = (value) => {
     const newTodo = [...todos, value];
     setTodos(newTodo);
   };
-  const [newInput, setnewInput] = useState();
+  const [newInput, setnewInput] = useState("");
   return (
     <div className="App">
-      <input type="text" className="input" onChange= {(e)=>{
-        setnewInput(e.target.value)
-      }}></input>
+      <div className = "content">
+      <input
+        type="text"
+        className="input"
+        onChange={(e) => {
+          setnewInput(e.target.value);
+        }}
+        value = {newInput}
+      ></input>
       <button
         onClick={() => {
           addTodo(newInput);
+          setnewInput("");
         }}
       >
         add
       </button>
+      </div>
       {todos.map((todo, index) => (
-        <Todo todo={todo} key={index} />
+        <Todo todo={todo} key={index} todos = {todos} setTodos = {setTodos} index = {index}/>
       ))}
     </div>
   );
--- day_21/src/components/todo.js
+++ day_21/src/components/todo.js
@@ -1,7 +1,15 @@
-const Todo = ({ todo }) => {
+const Todo = ({ todo, setTodos, todos, index }) => {
   return (
-    <div>
+    <div className="gg">
       <h1>{todo}</h1>
+      <button
+        onClick={() => {
+          const newTodos = todos.filter((el, i) => i != index);
+          setTodos(newTodos);
+        }}
+      >
+        delete
+      </button>
     </div>
   );
 };
