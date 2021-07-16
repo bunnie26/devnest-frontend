e8d14c915a54c2a8d821c63fd872935049e89e01
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-07-16 21:22:45 +0530

    random

--- day_21/src/App.js
+++ day_21/src/App.js
@@ -1,10 +1,29 @@
-
-import './App.css';
+import Todo from "./components/todo";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState(["how you doin"]);
+  const addTodo = (value) => {
+    const newTodo = [...todos, value];
+    setTodos(newTodo);
+  };
+  const [newInput, setnewInput] = useState();
   return (
     <div className="App">
-      
+      <input type="text" className="input" onChange= {(e)=>{
+        setnewInput(e.target.value)
+      }}></input>
+      <button
+        onClick={() => {
+          addTodo(newInput);
+        }}
+      >
+        add
+      </button>
+      {todos.map((todo, index) => (
+        <Todo todo={todo} key={index} />
+      ))}
     </div>
   );
 }
---
+++ day_21/src/components/todo.js
@@ -0,0 +1,9 @@
+const Todo = ({ todo }) => {
+  return (
+    <div>
+      <h1>{todo}</h1>
+    </div>
+  );
+};
+
+export default Todo;
