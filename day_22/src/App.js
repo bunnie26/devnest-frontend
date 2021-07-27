d5757913ee209596162ad45e322f031708693783
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-07-27 13:09:12 +0530

    completed day 22

--- day_22/src/App.js
+++ day_22/src/App.js
@@ -11,14 +11,13 @@ function App() {
     fetch("https://api.imgflip.com/get_memes").then((res) => {
       res.json().then((data) => {
         setTemplates(data.data.memes);
-        console.log(data.data.memes);
       });
     });
   }, []);
   return (
     <div className="App">
       <h1> Pro Meme Generator </h1>
-      {meme === null ? <Templates templates={templates} setMeme={setMeme} /> : <Meme meme={meme} />}
+      {meme === null ? <Templates templates={templates} setMeme={setMeme} /> : <Meme meme={meme} setMeme={setMeme} />}
     </div>
   );
 }
--- day_22/src/Meme.js
+++ day_22/src/Meme.js
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Meme({ meme }) {
+function Meme({ meme, setMeme }) {
+  const [form, setForm] = useState({
+    template_id: meme.id,
+    username: "teasty",
+    password: "PPS$Asr52ttCfaF",
+    boxes: [],
+  });
+  const generateMeme = () => {
+    let url = `https://api.imgflip.com/caption_image?template_id=${form.template_id}&username=${form.username}&password=${form.password}`;
+    form.boxes.map((box, index) => {
+      url += `&boxes[${index}][text]=${box.text}`;
+    });
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setMeme({...meme, url:data.data.url});
+      });
+  };
   return (
     <div className="meme">
       <img src={meme.url} />
       <div>
         {[...Array(meme.box_count)].map((_, index) => (
-          <input type="text" className="input" placeholder={`Meme Caption ${index+1}`} />
+          <input
+            key={index}
+            type="text"
+            className="input"
+            placeholder={`Meme Caption ${index + 1}`}
+            onChange={(e) => {
+              const newBoxes = form.boxes;
+              newBoxes[index] = { text: e.target.value };
+              setForm({ ...form, boxes: newBoxes });
+            }}
+          />
         ))}
       </div>
-      {/* <div className="buttons">
-        <button className="button">1</button>
-        <button>2</button>
-      </div> */}
+      <div className="buttons">
+        <button
+          className="button"
+          onClick={() => {
+            generateMeme();
+          }}
+        >
+          Generate Meme
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            setMeme(null);
+          }}
+        >
+          Choose Template
+        </button>
+      </div>
     </div>
   );
 }
