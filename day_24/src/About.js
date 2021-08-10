a44208b04b3f99c1b9c94d0436429d8e87834db5
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-10 23:55:20 +0530

    completed day 24

--- day_24/src/About.js
+++ day_24/src/About.js
@@ -6,7 +6,7 @@ function About() {
   const { login } = useContext(AppContext);
   return (
     <div className="About">
-      {login ? <h1>This is about section</h1> : <h1>Go and log in</h1>}
+      {login ? <h1>This is about section</h1> : <h1>You are not logged in, Go and log in</h1>}
       <p></p>
     </div>
   );
