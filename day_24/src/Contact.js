dd8961849621391e25b447472c52f9284630332b
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-10 18:36:30 +0530

    completed day 24

---
+++ day_24/src/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="Contact">
+      <h1>This is Contact section</h1>
+      <h2>+91 9212173314</h2>
+    </div>
+  );
+}
+
+export default Contact;
