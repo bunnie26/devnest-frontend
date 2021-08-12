b609b7dc24974c4aff20f1413fcd4e216eefd222
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-12 18:58:43 +0530

    day 26

---
+++ day-26/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Day 26</h1>
+      <input type="text" placeholder="Enter the song"></input>
+      <input type="text" placeholder="Name of artist"></input>
+    </div>
+  );
+}
+
+export default App;
