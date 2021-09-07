51b9e985286f90ba8ede4230c51d849afca997d8
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-09-08 01:39:45 +0530

    weather app AGAIN!!

--- day_28-29/src/App.js
+++ day_28-29/src/App.js
@@ -1,6 +1,16 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch(
+      "https://api.weatherapi.com/v1/current.json?key=40c4ae47ddf64f718a5131051210108&q=jaipur"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
   return (
     <div className="App">
       <h1>HELLO BROTHER</h1>
