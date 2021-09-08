e4c5a5b7ad8f8abdb0acbd3a044011b0b9b2dd2b
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-09-08 12:18:47 +0530

    randommm

--- day_28-29/src/App.js
+++ day_28-29/src/App.js
@@ -1,7 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { SearchIcon } from "@primer/octicons-react";
 import { useState, useEffect } from "react";
 
 function App() {
+  const [place, setPlace] = useState("");
+
   useEffect(() => {
     fetch(
       "https://api.weatherapi.com/v1/current.json?key=40c4ae47ddf64f718a5131051210108&q=jaipur"
@@ -13,7 +17,24 @@ function App() {
   });
   return (
     <div className="App">
-      <h1>HELLO BROTHER</h1>
+      <div className="container search">
+        <div className="row">
+          <div className="col-12 form">
+            <input
+              type="text"
+              placeholder="Enter City Name"
+              value={place}
+              onChange={(e) => {
+                setPlace(e.target.value);
+              }}
+            ></input>
+            <button className="btn btn-primary">
+              <SearchIcon size={18} />
+            </button>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
